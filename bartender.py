babb9d7e1f5011543ab94985fbb3eef8e87564d4
Jeremy Kemball <jkemball@gmail.com>
2017-08-29 15:27:15 -0400

    refactor into dictionary acceptor complete

--- bartender.py
+++ bartender.py
@@ -2,6 +2,7 @@ import yaml
 import sys
 import csv
 from collections import defaultdict
+from io import StringIO
 
 
 
@@ -21,6 +22,7 @@ example_layout ={
 
 
 example_media = {
+	#in M
 	"niacin":.1,
 	"biotin":.2,
 	"glucose":.01
@@ -28,7 +30,7 @@ example_media = {
 
 ex_goal={
 	"wells":96,
-	"vol":200,
+	"vol":200, #in umol
 	"name":"destination",
 	"excess":"water"
 }
@@ -47,11 +49,10 @@ def parse_layout(layfile):
 	return layout
 	# this should check for compatibility with a bunch of stuff
 	# does the layout file have insane volumes
-	# how much is there supposed to be?
 
 def leave_one_out(goal=ex_goal,media=example_media):
 
-
+	#do concentration here.
 	dest_wellnames = [goal["name"]+"."+offset_name(i) for i in range(0,goal["wells"])]
 	spec_dict = {dw:{} for dw in dest_wellnames}
 	sorted_cpds = sorted(media.keys())
@@ -61,7 +62,7 @@ def leave_one_out(goal=ex_goal,media=example_media):
 	for i,cpd in enumerate(sorted_cpds):
 		for j,wn in enumerate(sorted_wells):
 			if (j<num_replicates*i) or (j>= num_replicates*(i+1)):
-				spec_dict[wn][cpd]=media[cpd]
+				spec_dict[wn][cpd]=media[cpd]*goal["vol"]#in umols
 	return spec_dict
 
 def flatten(plan,layout):
@@ -86,68 +87,31 @@ def flatten(plan,layout):
 				return(source,amt)
 		print("not enough %s " % cpd)
 
-	
-
-def layout_CDM(layout,goal=ex_goal,media=example_media,filename="goals.csv"):
-	
-	def get_from_locations(cpd,moles,locations):
-		for i,loc in enumerate(locations[cpd]):
-			source,volume,molarity = loc
-			if volume*molarity>= moles:
-				amt = moles/molarity
-				nt = (source,volume-amt,molarity)
-				#i have to do it this way because tuples are immutable.
-				locations[cpd][i] = nt
-				return(source,amt)
-		print("not enough %s " % cpd)
-
-	totals = defaultdict(int)
-	locations = defaultdict(list)
-	for container in layout.keys():
-		for well,contents in layout[container].items():
-			totals[contents[0]]+=contents[1]*contents[2]
-			locations[contents[0]].append((
-				container+"."+well,
-				contents[1],
-				contents[2]))
-	num_cpds = len(media.keys())
-	num_replicates = int(ex_goal["wells"]/num_cpds)
-	#TODO: check for concentration problems
-	#like the compounds are too dilute
-	for compound in totals.keys():
-		reqd = media[compound]*(num_cpds-1)*num_replicates*goal["vol"]
-		have = totals[compound]
-		if reqd>have:
-			raise Exception("not enough %s, I need %s umol and I have %s umol"%(compound,reqd,have))
-	
-		
-	matrix = [{"wellname":goal["name"]+"."+offset_name(i)} for i in range(0,goal["wells"])]
-
-	sorted_cpds = sorted(media.keys())
-	allsources = ["wellname"]
-	for i,row in enumerate(matrix):
-		for j,cpd in enumerate(sorted_cpds):
-			#ugh here it is. This needs to be rewritten to take/make a big fat dictionary
-			# a1: "glucose":.1
-			#     "salt":.2
-			#     "water": to 200 uL
-			if (i<num_replicates*j) or (i>= num_replicates*(j+1)):
-				mols = media[cpd]*goal["vol"]
-				(source,amount) = get_from_locations(cpd,mols,locations)
-				row[source]=amount
-				if source not in allsources:
-					allsources.append(source)
-
-	with open(filename,'w') as goalfilep:
-		cwriter = csv.DictWriter(goalfilep,allsources,restval=0)
-		cwriter.writeheader()
-		for r in matrix:
-			cwriter.writerow(r)
+	matrix = [{"wellname":k} for k in plan.keys()]
+	allsources=["wellname"]
+	for m in matrix:
+		ingredients = plan[m["wellname"]]
+		for ing,umols in ingredients.items():
+			totals[ing]-=umols
+			if totals[ing]<0:
+				raise Exception("Not enough %s to complete a transfer spreadsheet." % ing)
+			(source,amt) = get_from_locations(ing,umols,locations)
+			if source not in allsources:
+				allsources.append(source)
+			m[source]=amt
+	fakefile = StringIO()
+	#this is silly.
+	cwriter = csv.DictWriter(fakefile,allsources,restval=0)
+	cwriter.writeheader()
+	for line in matrix:
+		cwriter.writerow(line)
+	return fakefile.getvalue()
 
 
 
 if __name__=="__main__":
-	print(yaml.dump(leave_one_out()))
+	spec_dict = leave_one_out()#default arguments test
+	print(yaml.dump(flatten(spec_dict,example_layout)))
 	#layout_CDM(example_layout)
 
 
