f0554c1e18851efac9bb7cb4342d38d45dce66fd
Jeremy Kemball <jkemball@gmail.com>
2017-10-23 16:42:44 -0400

    better proposals infrastructure

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -55,7 +55,6 @@ def sibling_sharing(media,dead_ingredients=[],oracle=lambda x: len(x)>0):
 		if not result:
 			dead_ingredients.append(ing)
 		else:
-			# god damn it dead_ingredients got propagated across and modified by its children's children
 			# the full slice [:] copies the list.
 			new_sol = sibling_sharing(smallmedia,dead_ingredients[:],oracle)
 			for sol in new_sol:
@@ -64,7 +63,7 @@ def sibling_sharing(media,dead_ingredients=[],oracle=lambda x: len(x)>0):
 	if len(solutions)==0:
 		#all are fatal, this is minimal
 		return [media]
-	minimal_size = min(map(len,solutions))
+	#minimal_size = min(map(len,solutions))
 	return solutions
 
 
@@ -75,22 +74,48 @@ def _stupid_oracle(media):
 		return True
 	return False
 
-def oracle(media, knowns):
+def oracle(media, knowns, proposed):
 	if media in knowns["no_growth"]:
 		return False
 	elif media in knowns["growth"]:
 		return True
 	else:
-		print( media)
+		proposed.append(media)
 		return False
 
 from functools import partial
-known_oracle = partial(oracle,knowns=example_knowns)
 
 if __name__=="__main__":
-	list_media=set(list(example_media.keys()))
-	print("full: ",list_media)
-	print("minimal ",sibling_sharing(list_media,[],known_oracle))
+	from sys import argv
+	import yaml
+
+	if len(argv)<2:
+		print("usage: branch_and_bound.py knowns.yaml media.yaml")
+		list_media=set(list(example_media.keys()))
+		print("full: ",list_media)
+		proposals = []
+		known_oracle = partial(oracle,knowns=example_knowns,proposed=proposals)
+		print("minimal ",sibling_sharing(list_media,[],known_oracle))
+		with open("example_media.yaml",'w') as yf:
+			yaml.dump(example_media,yf)
+		with open("example_knowns.yaml",'w') as yf:
+			yaml.dump(example_knowns,yf)
+		with open("example_proposed.yaml","w") as pf:
+			yaml.dump(proposals,pf)
+		print("proposed: ", "\n".join(map(str,proposals)))
+	else:
+		with open(argv[1],'r') as kf:
+			knowns = yaml.load(kf)
+		with open(argv[2],'r') as mf:
+			media = yaml.load(mf)
+		list_media = set(list(media.keys()))
+		proposals = []
+		known_oracle = partial(oracle,knowns=knowns,proposed=proposals)
+		print("minimal ",sibling_sharing(list_media,[],known_oracle))
+		print("proposed new experiments: ",proposals)
+		with open("new_proposed.yaml","w") as pf:
+			yaml.dump(proposals.pf)
+
 	#print(sibling_sharing(list_media))
 
 
